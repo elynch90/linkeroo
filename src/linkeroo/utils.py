7fba247e2987dff232151f81e6748ce1e286b7f5
Elliott <elynch90@gmail.com>
2024-09-05 23:30:17 -0700

    Update utils.py

--- src/linkeroo/utils.py
+++ src/linkeroo/utils.py
@@ -1,3 +1,4 @@
+from os import environ
 from pathlib import Path
 
 
@@ -12,12 +13,56 @@ def iter_path(path: Path) -> iter:
         yield path
 
 
-def format_static_link(fp: str) -> str:
-    """Format a static library file path for linking
-    :param fp: File path to format
-    :return: Formatted string"""
-    dir = fp.parent
-    file = fp.name
-    dir_str = f'-L{dir}'
-    file = file.replace('lib', '-l').replace('.a', '')
-    return f'{dir_str} {file}'
+def fetch_links(fp: str, suffix: str) -> list[str]:
+    p = Path(fp)
+    dir_path = p if p.is_dir() else p.parent
+    if not dir_path.exists():
+        raise ValueError("Directory does not exist")
+    links = []
+    dirs = [dir_path]
+    while dirs:
+        new_dirs = []
+        for item in dirs:
+            lib_paths = []
+            for path in item.iterdir():
+                if path.is_file() and path.suffix == suffix:
+                    lib_name = path.name.replace('lib', 'l').replace(suffix, '')
+                    lib_paths.append(lib_name)
+                elif path.is_dir():
+                    new_dirs.append(path)
+            if lib_paths:
+                links.append(f'-L{item} ' + ' '.join(f'-{lib}' for lib in lib_paths))
+        dirs = new_dirs
+    return links
+
+
+def traverse_dir(fp: Path, output, suffix: str, use_print: bool) -> None:
+    """Traverse a directory and write all static library files to a file
+    :param fp: Path to directory
+    :param output: Output file
+    :param suffix: File suffix
+    :param use_print: Print each path"""
+    links = fetch_links(fp, suffix)
+    # todo, pool together by common subdirectory -Lsubdir -> -lfile -lfile -lfile
+    # instead of -lsuperdir/file -lsuperdir/file
+    with open(output, 'w') as f:
+        for link in links:
+            if use_print:
+                print(f'{link}')
+            f.write(f'{link}\n')
+    print(f'Output written to {output}')
+
+
+def main() -> None:
+    fp = 'test/'
+    output = "linkeroo.txt"
+    suffix = ".a"
+    use_print = True
+    fp = Path(fp)
+    output = Path(output)
+    traverse_dir(fp, output, suffix, use_print)
+
+
+# test
+if __name__ == '__main__':
+    main()
